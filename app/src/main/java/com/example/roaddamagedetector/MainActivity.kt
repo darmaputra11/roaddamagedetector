7d8fb04fb765bf22d8b9fe339a02dbdaa3a3c621
darmaputra11 <iputra@student.unud.ac.id>
2025-03-06 17:13:43 +0800

    first fix try commit

--- app/src/main/java/com/example/roaddamagedetector/MainActivity.kt
+++ app/src/main/java/com/example/roaddamagedetector/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MaterialTheme {
                 Surface {
+                    sase
                     val navController = rememberNavController()
                     NavGraph(navController)
                 }
